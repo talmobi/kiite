c7f387e0227894a5e40794e27a174c71894b3503
Christian Talmo <talmo.christian@gmail.com>
2017-11-24 21:21:44 +0200

    refactored files

--- uid.js
+++
@@ -1,9 +0,0 @@
-var counter = 0
-var size = ( 1 << 16 )
-
-module.exports = function UID () {
-  var date = Date.now().toString( 16 ).slice( -10 )
-  var rnd = String( Math.floor( Math.random() * size ) )
-  return ( 'uid' + date + String( counter++ ) + rnd )
-}
-
